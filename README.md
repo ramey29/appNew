3094a258b8b13a040767a45ed8cf7e4129f83d4a
Rameshwar Bishnoi <rameshwarb@zeta.tech>
2019-03-16 20:21:21 +0530

    new commit

--- README.md
+++ README.md
@@ -18,5 +18,18 @@ npm run lint
 ### Customize configuration
 See [Configuration Reference](https://cli.vuejs.org/config/).
 
+### Faker API using JSON-server
 
 ### use json-server locally to run the project in your system and use the db.json file added here in root
+
+## To install server
+npm install -g json-server
+
+## Copy the db.json file attached with this repo in root
+keep th db.json file in root of that folder 
+
+## To run json-server
+npm run json-server --watch db.json 
+
+## check if server is running at :3000 port
+ http://localhost:3000/ good to go , if not change the local env in env.js file as per the port the server is running
--- src/components/ConversionHistory.vue
+++ src/components/ConversionHistory.vue
@@ -3,7 +3,7 @@
         <div class="searchConatiner">
             <div class="searchInner">
                 <div class="w100">
-                    <input id="searchInput" @keyup="Emptysearch" v-on:keyup.enter="search(searchTerm)" v-on:keyup="autocomplete()" type="text" class="" v-model="searchTerm" placeholder="Search for restaurants near you" style="border: unset;" autocomplete="on" />
+                    <input @keyup="Emptysearch" v-on:keyup.enter="search(searchTerm)" v-on:keydown.enter="Emptysearch()"  type="text" class="" v-model="searchTerm" placeholder="Search for restaurants near you" autocomplete="on" id="searchInput"/>
                     <div class="btnDv">
                     <button class="btn new-btn" type="button" @click="search(searchTerm)">
                         <span class="search-icon" >SEARCH</span>
@@ -46,7 +46,7 @@
 import AppConversionService from './../service/appConversionService';
 import Utils from './../common/Utils';
 import { publishEvent } from '../common/Observer';
-// import {autocomplete} from "./../service/autoComplete"
+import {autocomplete} from "./../service/autoComplete"
 
 
 export default {
@@ -60,6 +60,7 @@ export default {
             tags:[],
             tagsBoolean:false,
             order:'asc',
+            autocompleteVar: ["pizzas", "pastry"],
             page: 1,
             limit: 40,
             totalCount: 30
@@ -70,13 +71,16 @@ export default {
     },
     created() {
         this.fetchTransactions(this.limit, this.page, null, null);
-        // this.autocomplete(document.getElementById("searchInput"), this.restaurants);
     },
     mounted(){
           
             
     },
-    methods: {
+    methods: { 
+        autoComplete(){
+            autocomplete(document.getElementById("searchInput"), this.autocompleteVar);
+        },
+        
         close($event,index){
             $event.target.parentNode.remove();
             this.tags.splice(index,1);
@@ -103,6 +107,7 @@ export default {
                     this.restaurants = response.data;
                     this.totalCount =  response.headers["x-total-count"];
                     localStorage.setItem('restaurants', JSON.stringify(this.restaurants));
+                    
                 })
                 .catch(error => {
               
@@ -123,11 +128,15 @@ export default {
             let searchText = text.toLowerCase();
             for (var i = 0; i < this.restaurants.length; i++) {
             let name = this.restaurants[i].name.toLowerCase().search(searchText);
+            
             const abc = this.restaurants[i].foodtype.filter(foodtypes => foodtypes.toLowerCase().indexOf(searchText) > -1);
             if (name > -1 || abc.length > 0) {
-                searchResults.push(this.restaurants[i])
+                searchResults.push(this.restaurants[i]);
+                this.autocompleteVar.push(this.restaurants[i].name);
             }
             }
+            this.autocompleteVar.push(text);
+            localStorage.setItem('autocompleteVar', JSON.stringify(this.autocompleteVar));
             this.noSearchResults = (searchResults.length == 0) ? true : false
             this.restaurants = [...searchResults];
       },
@@ -136,106 +145,12 @@ export default {
            if (this.searchTerm != '') {} else {
             this.noSearchResults = false;
             this.showViewAll = 'true';
+            this.tagsBoolean =  false;
             this.fetchTransactions(this.limit, this.page, null, null);
             }
         },
       
-        autocomplete(inp, arr) {
-            /*the autocomplete function takes two arguments,
-            the text field element and an array of possible autocompleted values:*/
-            var currentFocus;
-            /*execute a function when someone writes in the text field:*/
-            inp.addEventListener("input", function(e) {
-                var a, b, i, val = this.value;
-                /*close any already open lists of autocompleted values*/
-                closeAllLists();
-                if (!val) { return false;}
-                currentFocus = -1;
-                /*create a DIV element that will contain the items (values):*/
-                a = document.createElement("DIV");
-                a.setAttribute("id", this.id + "autocomplete-list");
-                a.setAttribute("class", "autocomplete-items");
-                /*append the DIV element as a child of the autocomplete container:*/
-                this.parentNode.appendChild(a);
-                /*for each item in the array...*/
-                for (i = 0; i < arr.length; i++) {
-                /*check if the item starts with the same letters as the text field value:*/
-                if (arr[i].substr(0, val.length).toUpperCase() == val.toUpperCase()) {
-                    /*create a DIV element for each matching element:*/
-                    b = document.createElement("DIV");
-                    /*make the matching letters bold:*/
-                    b.innerHTML = "<strong>" + arr[i].substr(0, val.length) + "</strong>";
-                    b.innerHTML += arr[i].substr(val.length);
-                    /*insert a input field that will hold the current array item's value:*/
-                    b.innerHTML += "<input type='hidden' value='" + arr[i] + "'>";
-                    /*execute a function when someone clicks on the item value (DIV element):*/
-                    b.addEventListener("click", function(e) {
-                        /*insert the value for the autocomplete text field:*/
-                        inp.value = this.getElementsByTagName("input")[0].value;
-                        /*close the list of autocompleted values,
-                        (or any other open lists of autocompleted values:*/
-                        closeAllLists();
-                    });
-                    a.appendChild(b);
-                }
-                }
-            });
-            /*execute a function presses a key on the keyboard:*/
-            inp.addEventListener("keydown", function(e) {
-                var x = document.getElementById(this.id + "autocomplete-list");
-                if (x) x = x.getElementsByTagName("div");
-                if (e.keyCode == 40) {
-                /*If the arrow DOWN key is pressed,
-                increase the currentFocus variable:*/
-                currentFocus++;
-                /*and and make the current item more visible:*/
-                addActive(x);
-                } else if (e.keyCode == 38) { //up
-                /*If the arrow UP key is pressed,
-                decrease the currentFocus variable:*/
-                currentFocus--;
-                /*and and make the current item more visible:*/
-                addActive(x);
-                } else if (e.keyCode == 13) {
-                /*If the ENTER key is pressed, prevent the form from being submitted,*/
-                e.preventDefault();
-                if (currentFocus > -1) {
-                    /*and simulate a click on the "active" item:*/
-                    if (x) x[currentFocus].click();
-                }
-                }
-            });
-            function addActive(x) {
-            /*a function to classify an item as "active":*/
-            if (!x) return false;
-            /*start by removing the "active" class on all items:*/
-            removeActive(x);
-            if (currentFocus >= x.length) currentFocus = 0;
-            if (currentFocus < 0) currentFocus = (x.length - 1);
-            /*add class "autocomplete-active":*/
-            x[currentFocus].classList.add("autocomplete-active");
-            }
-            function removeActive(x) {
-            /*a function to remove the "active" class from all autocomplete items:*/
-            for (var i = 0; i < x.length; i++) {
-                x[i].classList.remove("autocomplete-active");
-            }
-            }
-            function closeAllLists(elmnt) {
-            /*close all autocomplete lists in the document,
-            except the one passed as an argument:*/
-            var x = document.getElementsByClassName("autocomplete-items");
-            for (var i = 0; i < x.length; i++) {
-                if (elmnt != x[i] && elmnt != inp) {
-                x[i].parentNode.removeChild(x[i]);
-                }
-            }
-            }
-            /*execute a function when someone clicks in the document:*/
-            document.addEventListener("click", function (e) {
-                closeAllLists(e.target);
-            });
-    }
+        
     },
     computed:{
         
@@ -247,7 +162,7 @@ export default {
 .mainContainer {width:100%;
    .searchConatiner{background:url(./../assets/img/foodBg.jpg) no-repeat;background-position:100% 50%;width:100%;height:250px;background-size:cover;position:relative;
     .searchInner{position:absolute;top:50%;left:50%;transform: translate(-50%,-50%);min-width:60%;}
-    input{width:80%;}
+    input{width:80%;border: unset;position:relative;}
     .btnDv{display: inline-block;width:16%;padding-left:20px;}
     .searchResultDv{margin-top: 20px;background: lightgrey;padding: 10px;
         span{display:inline-block;background: #fff;padding:2px 4px;margin-right:4px;
@@ -272,8 +187,5 @@ export default {
        .btnDv{display: inline-block;width:12%;padding-left:20px;}
        }
    }
-  
-
 }
-
 </style>
